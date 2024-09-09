85e114f0fffdcb1637149b2f14cf88f101489b3e
maxADeVisser <m.madv96@gmail.com>
2024-09-09 16:22:30 +0200

    updated configuration

--- fiftyone_test.py
+++
@@ -1,7 +0,0 @@
-# Test script to play around with FiftyOne interface
-import fiftyone as fo
-import fiftyone.zoo as foz
-
-dataset = foz.load_zoo_dataset("quickstart")
-session = fo.launch_app(dataset)
-session.wait()
