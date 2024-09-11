6fd36903b6d1ed8f0f659d17bdc9b6528ed353b6
maxADeVisser <m.madv96@gmail.com>
2024-09-11 16:49:29 +0200

    doing work on analysis dicom meta data

---
+++ project_config.py
@@ -0,0 +1,30 @@
+"""Project config file"""
+
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv(".env")
+
+
+class _Config:
+    """Config class for the project. Intended to be a singleton."""
+
+    def __init__(self):
+        self.DATA_DIR = os.getenv("LIDC_IDRI_DIR")
+        self.patient_ids = sorted(
+            [
+                pid
+                for pid in os.listdir(self.DATA_DIR)
+                if os.path.isdir(os.path.join(self.DATA_DIR, pid))
+            ]
+        )
+
+
+# Singleton pattern: only one instance of the Config class is created
+config = _Config()
+
+if __name__ == "__main__":
+    config = _Config()
+    print(config.DATA_DIR)
+    print(len(config.patient_ids))
