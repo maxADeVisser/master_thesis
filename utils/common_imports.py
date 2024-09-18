63fc4b74728d33b30d6c82a1eca10396ecf69ddb
maxADeVisser <m.madv96@gmail.com>
2024-09-18 15:34:10 +0200

    checkpoint before changing logic

--- utils/common_imports.py
+++ utils/common_imports.py
@@ -1,6 +1,7 @@
 """Module to import common libraries and modules for the project."""
 
 import os
+from typing import Any, Literal
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -8,6 +9,7 @@ import pandas as pd
 import pydicom
 import pylidc as pl
 import seaborn as sns
+from tqdm import tqdm
 
 from project_config import config
 
