f42add5945971e02af6a24ff6a0daf4785783e0b
maxADeVisser <m.madv96@gmail.com>
2024-09-24 11:38:37 +0200

    got interactive visualisation of the bboxes for the dataloader to work

--- model/dataset.py
+++ model/dataset.py
@@ -4,7 +4,6 @@ import os
 
 import numpy as np
 import pandas as pd
-import pylidc as pl
 import torch
 from torch.utils.data import Dataset
 from torchvision import transforms
@@ -12,28 +11,38 @@ from torchvision import transforms
 from project_config import config
 from utils.pylidc_utils import get_scans_by_patient_id
 
+nodule_df_path = "out/nodule_df_all_pad_10.csv"
+
 
 class LIDC_IDRI_DATASET(Dataset):
+    """
+    NOTE: Relies on having the nodule dataframe created by create_nodule_df.py script
+    """
+
     def __init__(self, main_dir_path: str = config.DATA_DIR) -> None:
         self.main_dir_path = main_dir_path
         self.patient_ids = config.patient_ids
 
         assert os.path.exists(
-            "out/nodule_df.csv"
+            nodule_df_path
         ), "Nodule dataframe not found. Please run create_nodule_df.py script to create"
 
-        self.nodule_df = pd.read_csv("out/nodule_df.csv").assign(
-            consensus_bbox=lambda x: x["consensus_bbox"].apply(ast.literal_eval)
+        # Read in the nodule dataframe and convert the string representations to python objects
+        self.nodule_df = pd.read_csv(nodule_df_path).assign(
+            consensus_bbox=lambda x: x["consensus_bbox"].apply(ast.literal_eval),
+            nodule_annotation_ids=lambda x: x["nodule_annotation_ids"].apply(
+                ast.literal_eval
+            ),
         )
 
-        self.transform = transforms.Compose(transforms.ToTensor())
+        # self.transform = transforms.Compose(transforms.ToTensor())
 
     def __len__(self) -> int:
         return len(self.nodule_df)
 
     def __getitem__(self, idx: int) -> torch.Tensor:
         """Retrieves a single nodule roi in size 64x64x64 and its cancer label"""
-        # TODO standardise the size of the nodule roi
+        # TODO standardise the size of the nodule roi to 64x64x64
         nodule_row = self.nodule_df.iloc[idx]
         scan: np.ndarray = get_scans_by_patient_id(nodule_row["pid"], to_numpy=True)
         x_bounds, y_bounds, z_bounds = nodule_row["consensus_bbox"]
@@ -51,13 +60,14 @@ class LIDC_IDRI_DATASET(Dataset):
 
 # %%
 if __name__ == "__main__":
+    # roi_consensus, label = dataset.__getitem__(nodule_idx)
+
+    # VISUAL INSPECTION OF BOUNDING BOX AND MASK:
+    from utils.visualisation import interactive_nodule_bbox_visualisation
+
     dataset = LIDC_IDRI_DATASET()
-    dataset.nodule_df
+    interactive_nodule_bbox_visualisation(
+        nodule_df=dataset.nodule_df, nodule_idx=0, annotation_idx=0
+    )
 
-    idx = 700
-    roi, label = dataset.__getitem__(idx)
-    import matplotlib.pyplot as plt
-
-    plt.imshow(roi[:, :, 0], cmap="gray")
-    plt.title(f"Label: {label}")
-    plt.show()
+# %%
---
+++ preprocessing/create_nodule_df.py
@@ -0,0 +1,110 @@
+"""Scrip for creating a pandas dataframe with nodule data and malignancy information"""
+
+# %%
+from statistics import median_high
+
+from pylidc.utils import consensus
+
+from utils.common_imports import *
+from utils.logger_setup import logger
+
+# SCRIPT PARAMS:
+# confidence level: A pixel will be considered part of the nodule if it is annotated by at least @c_level of the radiologists
+c_level = 0.5
+padding = 10
+verbose = False
+csv_file_name = "nodule_df_all_pad_10"
+
+
+def compute_nodule_malignancy(nodule: pl.Annotation) -> str:
+    """
+    Compute the malignancy of a nodule with the annotations made by 4 doctors.
+    Return median high of the annotated cancer, True or False label for cancer,
+    (source: https://github.com/benkeel/VAE_lung_lesion_BMVC/blob/main/Preprocessing/LIDC_DICOM_to_Numpy.ipynb)
+    """
+    malignancy = median_high([ann.malignancy for ann in nodule])
+    if malignancy > 3:
+        return malignancy, "Malignant"
+    elif malignancy < 3:
+        return malignancy, "Benign"
+    else:
+        return malignancy, "Ambiguous"
+
+
+def main() -> None:
+    dict_df = {}
+    for pid in tqdm(config.patient_ids[:100]):
+        scan = pl.query(pl.Scan).filter(pl.Scan.patient_id == pid).all()
+        if len(scan) > 1:
+            logger.debug(f"A patient {pid} has more than one scan: {len(scan)}")
+        scan = scan[0]
+
+        # Get the annotations for the individual nodules in the scan:
+        nodules_annotation: list[list[pl.Annotation]] = scan.cluster_annotations(
+            verbose=verbose
+        )
+
+        # TODO add the size of the nodule from the nodules size list
+        # TODO we can also exlude nodules if they only have a single annotation?
+        if len(nodules_annotation) >= 1:
+            for nodule_idx, nodule_anns in enumerate(nodules_annotation):
+
+                # Get the consensus mask and bbox at @c_level consensus from the 4 radiologists
+                # Refer to documentation for more information
+                consensus_mask, cmbbox = consensus(
+                    anns=nodule_anns, clevel=c_level, pad=padding, ret_masks=False
+                )
+                x = (int(cmbbox[0].start), int(cmbbox[0].stop))
+                y = (int(cmbbox[1].start), int(cmbbox[1].stop))
+                z = (int(cmbbox[2].start), int(cmbbox[2].stop))
+
+                # Calculate the malignancy of the nodule:
+                malignancy_score, cancer_label = compute_nodule_malignancy(nodule_anns)
+
+                # TODO there is something wrong with the consensus_bbox, it is not returning the bbox in 3D.
+                dict_df[f"{nodule_idx}_{pid}"] = {
+                    "pid": pid,
+                    "nodule_idx": nodule_idx,
+                    "malignancy_score": malignancy_score,
+                    "cancer_label": cancer_label,
+                    "consensus_bbox": (x, y, z),
+                    "nodule_annotation_ids": tuple(
+                        [int(ann.id) for ann in nodule_anns]
+                    ),
+                    "padding": padding,
+                }
+
+                # TODO might need to do some more processing here...
+                # i.e. check if the mask is too small to be included (this can maybe be done using the nodules size list from provided as augmentation for the dataset)
+                # for nodule in range(consensus_mask.shape[-1]):
+                #     if np.sum(consensus_mask[:, :, nodule]) <= mask_threshold:
+                #         continue
+
+    nodule_df = pd.DataFrame.from_dict(dict_df, orient="index")
+
+    # Filtering:
+    # nodule_df = nodule_df[nodule_df["cancer_label"] != "Ambiguous"]
+
+    # Type casting
+    nodule_df = nodule_df.assign(
+        pid=nodule_df["pid"].astype("string"),
+        nodule_idx=nodule_df["nodule_idx"].astype("int"),
+        malignancy_score=nodule_df["malignancy_score"].astype("int"),
+        cancer_label=nodule_df["cancer_label"].astype("category"),
+    )
+
+    try:
+        nodule_df.to_csv(f"{config.OUT_DIR}/{csv_file_name}.csv")
+    except Exception as e:
+        logger.error(f"Error saving nodule_df dataframe: {e}")
+
+
+# %%
+if __name__ == "__main__":
+    main()
+
+    # # DEBUGGING:
+    # nodule_df = pd.read_csv(f"{config.OUT_DIR}/{csv_file_name}.csv")
+    # nodule_df
+
+# %%
