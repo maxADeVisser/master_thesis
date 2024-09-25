197229590859ff91cae68eab1816b667e6cf7649
maxADeVisser <m.madv96@gmail.com>
2024-09-25 15:08:25 +0200

    added hounsfield distribution plot

--- utils/visualisation.py
+++ utils/visualisation.py
@@ -1,11 +1,29 @@
 # %%
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
-import pylidc as pl
-from pylidc.utils import volume_viewer
+from tqdm import tqdm
 
-from model.dataset import LIDC_IDRI_DATASET
+from project_config import config
+from utils.pylidc_utils import get_scans_by_patient_id
+
+
+def plot_scan_hounsfield_histogram(pids: list[str], bins: int = 80) -> None:
+    """
+    Plots the histogram of Hounsfield units in a CT scan
+    @scan - a 3D numpy array of the CT scan
+    """
+    scan = get_scans_by_patient_id(pids[0], to_numpy=True)
+
+    for pid in tqdm(range(1, len(pids))):
+        next_pid_scan = get_scans_by_patient_id(config.patient_ids[pid], to_numpy=True)
+        scan = np.concatenate([scan, next_pid_scan], axis=2)
+
+    print("Plotting...")
+    plt.title(f"Hounsfield Units (HU) Distribution for {len(pids)} Scans")
+    plt.hist(scan.flatten(), bins=bins, color="c")
+    plt.xlabel("Hounsfield Units (HU)")
+    plt.ylabel("Frequency")
+    plt.show()
 
 
 def plot_slices(
@@ -49,6 +67,7 @@ def plot_slices(
     plt.show()
 
 
+# %%
 if __name__ == "__main__":
     # TESTING
     """Need to Load a scan from a folder and returns a 3D numpy array of all the scans stacked together
@@ -63,4 +82,5 @@ if __name__ == "__main__":
         # save_path="out/test.png",
     )
 
+    plot_scan_hounsfield_histogram(config.patient_ids[:20], bins=80)
 # %%
