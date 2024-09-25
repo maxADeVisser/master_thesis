c12f05438f12ff2e821904a32c65cbe55c8cf67f
maxADeVisser <m.madv96@gmail.com>
2024-09-25 15:29:14 +0200

    updated nodule dataframe script and added nodule dataframe eda

--- preprocessing/create_nodule_df.py
+++ preprocessing/create_nodule_df.py
@@ -9,28 +9,31 @@ from utils.logger_setup import logger
 
 SCRIPT_PARAMS = pipeline_config["preprocessing"]["nodule_df"]
 image_dim = SCRIPT_PARAMS["image_dim"]
-csv_file_name = SCRIPT_PARAMS["nodule_df_csv_name"]
+# csv_file_name = SCRIPT_PARAMS["nodule_df_csv_name"]
+csv_file_name = "test"
 verbose = False
 
 
-def compute_nodule_malignancy(nodule: pl.Annotation) -> str:
+def get_malignancy_label(malignancy_scores: tuple[int]) -> str:
     """
-    Compute the malignancy of a nodule with the annotations made by 4 doctors.
-    Return median high of the annotated cancer, True or False label for cancer,
+    Return the consensun malignancy label based on the median high of the annotations.
+    If the median high is greater than 3, the nodule is labelled as "Malignant".
+    If the median high is less than 3, the nodule is labelled as "Benign".
+    If the median high is equal to 3, the nodule is labelled as "Ambiguous".
     (source: https://github.com/benkeel/VAE_lung_lesion_BMVC/blob/main/Preprocessing/LIDC_DICOM_to_Numpy.ipynb)
     """
-    malignancy = median_high([ann.malignancy for ann in nodule])
-    if malignancy > 3:
-        return malignancy, "Malignant"
-    elif malignancy < 3:
-        return malignancy, "Benign"
+    consensus_malignancy = median_high(malignancy_scores)
+    if consensus_malignancy > 3:
+        return "Malignant"
+    elif consensus_malignancy < 3:
+        return "Benign"
     else:
-        return malignancy, "Ambiguous"
+        return "Ambiguous"
 
 
 def main() -> None:
     dict_df = {}
-    for pid in tqdm(config.patient_ids):
+    for pid in tqdm(config.patient_ids[:10]):
         scan = pl.query(pl.Scan).filter(pl.Scan.patient_id == pid).all()
         if len(scan) > 1:
             logger.debug(f"A patient {pid} has more than one scan: {len(scan)}")
@@ -41,7 +44,7 @@ def main() -> None:
             verbose=verbose
         )
 
-        # TODO we can also exlude nodules if they only have a single annotation?
+        # TODO we can also exlude nodules if they only have a single annotation? (there are a significant portion)
         if len(nodules_annotation) >= 1:
             for nodule_idx, nodule_anns in enumerate(nodules_annotation):
 
@@ -68,47 +71,42 @@ def main() -> None:
                     consensus_centroid[2] + (image_dim // 2),
                 )
 
-                # Compute consensus diameter of the nodule:
-                ann_mean_diameter = np.mean([ann.diameter for ann in nodule_anns])
-
-                # Compute consensus volumne of the nodule:
-                ann_mean_volume = np.mean([ann.volume for ann in nodule_anns])
-
-                # Calculate the malignancy of the nodule:
-                malignancy_score, cancer_label = compute_nodule_malignancy(nodule_anns)
-
                 dict_df[f"{nodule_idx}_{pid}"] = {
                     "pid": pid,
                     "nodule_idx": nodule_idx,
-                    "malignancy_score": malignancy_score,
-                    "cancer_label": cancer_label,
                     "consensus_centroid": consensus_centroid,
+                    "bbox_dim": image_dim,
                     "consensus_bbox": (x_dim, y_dim, z_dim),
-                    "ann_mean_diameter": ann_mean_diameter,
-                    "ann_mean_volume": ann_mean_volume,
+                    "ann_mean_diameter": np.mean([ann.diameter for ann in nodule_anns]),
+                    "ann_mean_volume": np.mean([ann.volume for ann in nodule_anns]),
                     "nodule_annotation_ids": tuple(
                         [int(ann.id) for ann in nodule_anns]
                     ),
                     "nodule_annotation_count": len(nodule_anns),
-                    "image_dim": image_dim,
+                    "malignancy_scores": tuple([ann.malignancy for ann in nodule_anns]),
                 }
 
                 # TODO might need to do some more processing here...
-                # i.e. check if the mask is too small to be included (this can maybe be done using the nodules size list from provided as augmentation for the dataset)
-                # for nodule in range(consensus_mask.shape[-1]):
-                #     if np.sum(consensus_mask[:, :, nodule]) <= mask_threshold:
-                #         continue
+                # i.e. check if the mask is too small to be included
 
     nodule_df = pd.DataFrame.from_dict(dict_df, orient="index")
 
+    nodule_df["malignancy_consensus"] = nodule_df["malignancy_scores"].apply(
+        median_high
+    )
+    nodule_df["cancer_label"] = nodule_df["malignancy_scores"].apply(
+        get_malignancy_label
+    )
+
     # FILTERING:
     # nodule_df = nodule_df[nodule_df["cancer_label"] != "Ambiguous"]
+    # ...
 
     # TYPE CASTING
     nodule_df = nodule_df.assign(
         pid=nodule_df["pid"].astype("string"),
         nodule_idx=nodule_df["nodule_idx"].astype("int"),
-        malignancy_score=nodule_df["malignancy_score"].astype("int"),
+        malignancy_consensus=nodule_df["malignancy_consensus"].astype("int"),
         cancer_label=nodule_df["cancer_label"].astype("category"),
     )
 
@@ -138,7 +136,7 @@ def main() -> None:
 
     # WRITE FILE:
     try:
-        nodule_df.to_csv(f"{config.OUT_DIR}/{csv_file_name}.csv")
+        nodule_df.to_csv(f"{config.OUT_DIR}/{csv_file_name}.csv", index=True)
     except Exception as e:
         logger.error(f"Error saving nodule_df dataframe: {e}")
 
