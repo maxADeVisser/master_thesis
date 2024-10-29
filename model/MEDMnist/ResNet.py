25bd3db882f9c5c58ad86e48318b0d11185112ee
maxADeVisser <m.madv96@gmail.com>
2024-10-29 22:18:39 +0100

    recfactored model to use Sequential and added func for feature vectors

--- model/MEDMnist/ResNet.py
+++ model/MEDMnist/ResNet.py
@@ -89,23 +89,22 @@ class ResNet(nn.Module):
         """
         super(ResNet, self).__init__()
         self.in_planes = 64
-
-        # Initial Conv Layer (no downsampling):
-        self.conv1 = nn.Conv2d(
-            in_channels, 64, kernel_size=3, stride=1, padding=1, bias=False
+        self.features = nn.Sequential(
+            nn.Conv2d(in_channels, 64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            # nn.GroupNorm(num_groups=2, num_channels=64),
+            self._make_layer(64, num_blocks[0], stride=1),
+            self._make_layer(128, num_blocks[1], stride=2),
+            self._make_layer(256, num_blocks[2], stride=2),
+            self._make_layer(512, num_blocks[3], stride=2),
+            nn.AdaptiveAvgPool2d((1, 1)),
         )
-        self.bn1 = nn.BatchNorm2d(64)
-        # self.bn1 = nn.GroupNorm(num_groups=2, num_channels=64)
-        self.layer1 = self._make_layer(64, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(128, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(256, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(512, num_blocks[3], stride=2)
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.corn_output_layer = nn.Linear(
+        self.corn_classifier_layer = nn.Linear(
             in_features=512 * Bottleneck.expansion, out_features=num_classes - 1
         )
 
-    def _make_layer(self, planes: int, num_blocks: int, stride: int):
+    def _make_layer(self, planes: int, num_blocks: int, stride: int) -> nn.Sequential:
         """
         Create a layer with multiple blocks
         @planes is the number of output channels of the first block in the layer
@@ -124,18 +123,20 @@ class ResNet(nn.Module):
             self.in_planes = planes * Bottleneck.expansion
         return nn.Sequential(*layers)
 
-    def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = self.layer1(out)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
-        # out = F.avg_pool2d(out, 4)
-        # out = F.adaptive_avg_pool3d(out, output_size=4)
-        out = self.avgpool(out)
-        flattened = out.view(out.size(0), -1)
-        logits = self.corn_output_layer(flattened)
+    def forward(self, x) -> torch.Tensor:
+        features = self.features(x)
+        flattened = features.view(features.size(0), -1)
+        logits = self.corn_classifier_layer(flattened)
         return logits
+
+    def get_feature_vector(self, x) -> torch.Tensor:
+        """
+        Return the feature vector before the classifier layer.
+        Returns tensor of shape (batch_size, 512 * Bottleneck.expansion)
+        """
+        with torch.no_grad():
+            # flatten the images in the batch
+            return self.features(x).view(x.size(0), -1)
 
 
 def ResNet50(in_channels, num_classes) -> ResNet:
@@ -243,6 +244,9 @@ if __name__ == "__main__":
     model = ResNet50(in_channels=1, num_classes=5)  # 2D model
     model = convert_model_to_3d(model)  # 3D model
     test_input = torch.randn(batch_size, channels, img_dim, img_dim, img_dim)
+
+    features = model.get_feature_vector(test_input)
+
     logits = model(test_input)
     logits
 
