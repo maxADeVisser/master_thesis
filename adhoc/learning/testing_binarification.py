5da68d76e38223557d79bd4b459d5654cc557e93
maxADeVisser <m.madv96@gmail.com>
2024-10-29 16:11:28 +0100

    updated dependencies

--- adhoc/learning/testing_binarification.py
+++ adhoc/learning/testing_binarification.py
@@ -2,58 +2,51 @@
 import torch
 from coral_pytorch.dataset import corn_label_from_logits
 
+from model.MEDMnist.ResNet import compute_class_probs_from_logits
 from utils.common_imports import *
 
 
 def plot_prediction(rank_predictions: list[float]) -> None:
-    plt.bar(np.arange(len(rank_predictions)), rank_predictions)
+    plt.bar(x=np.arange(len(rank_predictions)), height=rank_predictions)
     plt.ylim(0, 1)
     plt.hlines(
-        y=predictions_threshold,
-        xmin=0,
-        xmax=len(rank_predictions),
+        y=0.5,
+        xmin=0 - 0.5,
+        xmax=len(rank_predictions) - 1 + 0.5,
         colors="r",
         linestyles="--",
         label="50% threshold",
     )
+    labels = [f"P(X>{i + 1})" for i, x in enumerate(rank_predictions)]
+    plt.xticks(np.arange(len(rank_predictions)), labels=labels)
     plt.legend()
     plt.show()
 
 
-out1 = [-0.6, 0.7, 0.4, 0.3]
-out2 = [0.7, 0.6, 0.5, 0.4]
-predictions_threshold = 0.5
-logits = torch.Tensor(
-    [
-        out1,
-        out2,
-    ]
-)
+out1 = [0.9, 0.7, 0.4, 0.3]  # should predict rank 3
+out2 = [0.7, 0.6, 0.5, 0.4]  # should predict rank 4
+out3 = [0.4, 0.3, 0.2, 0.1]  # should predict rank 1
+sigmoid_output = torch.Tensor([out1, out2, out3])
 
+compute_class_probs_from_logits(sigmoid_output)
 
-def binary_inference(model_output):
-    # model_output[0] corresponds to P(y > 1)
-    # model_output[1] corresponds to P(y > 2)
-    # model_output[2] corresponds to P(y > 3)
-    # model_output[3] corresponds to P(y > 4)
-
-    threshold = 0.5
-    if (
-        model_output[2] >= threshold
-    ):  # Change 2 to 1 if you want to classify for score >= 2
-        return 1  # Malignant
-    else:
-        return 0  # Benign
-
-
-binary_inference(out2)
-
-# corn_label_from_logits(output)
-probas = torch.sigmoid(logits)
-probas = torch.cumprod(probas, dim=1)
+# corn_label_from_logits(logits)
+probas = torch.cumprod(sigmoid_output, dim=1)  # cumulative probability
 predict_levels = probas > 0.5
-predicted_labels = torch.sum(predict_levels, dim=1)
+predicted_ranks = torch.sum(predict_levels, dim=1) + 1
 
-plot_prediction(out2)
+plot_prediction(out3)
+
+
+with torch.no_grad():
+    probas = torch.cumprod(probas, dim=1)
+    predicted_rank = torch.sum(probas > 0.5, dim=1) + 1
+
+# understanding cumprod func
+test = torch.Tensor([[2, 3, 4, 5]])
+torch.cumprod(test, dim=1)
+
+# generate 100 normally distribution samples between 0 and 1
+probas = np.random.normal(0.5, 0.2, 100)
 
 # %%
