c33b976c1c1b606c46d792fd1d3acea414abd3aa
Max de Visser <m.madv96@gmail.com>
2024-11-12 14:29:07 +0100

    updated sync script

--- hpc/fetch_hpc_data.py
+++ hpc/fetch_hpc_data.py
@@ -9,8 +9,8 @@ def _transfer_hpc_to_local(hpc_path: str, local_path: str) -> None:
     try:
         subprocess.run(["scp", f"{hpc_path}", f"{local_path}"], check=True)
         print("Data fetched successfully (Y)")
-    except subprocess.CalledProcessError as e:
-        print(f"Error: {e}")
+    except subprocess.CalledProcessError as _:
+        print(f"There was an error (maybe the file is not there)")
 
 
 def get_job_stdout(job_id: int, dest_dir: str) -> None:
@@ -33,27 +33,20 @@ def update_loss_plot(experiment_id: str) -> None:
     _transfer_hpc_to_local(hpc_out, out_dir)
 
 
-# def update_error_plot(experiment_id: str) -> None:
-#     """
-#     Fetch the latest loss plot from the HPC
-#     """
-#     hpc_out = f"maxd@hpc.itu.dk:~/master_thesis/out/model_runs/{experiment_id}/error_distribution.png"
-#     out_dir = f"/Users/maxvisser/Documents/ITU/master_thesis/hpc/jobs/{experiment_id}"
-#     _transfer_hpc_to_local(hpc_out, out_dir)
-
-
-def fetch_model_weights(job_id: int, dest_dir: str) -> None:
+def fetch_model_weights(experiment_id: str) -> None:
     """
     Fetch the model weights from the HPC
     """
-    hpc_out = f"maxd@hpc.itu.dk:~/master_thesis/out/model_runs"
-    _transfer_hpc_to_local(hpc_out, dest_dir)
+    hpc_out = f"maxd@hpc.itu.dk:~/master_thesis/out/model_runs/{experiment_id}/model_fold_0.pth"
+    out_dir = f"/Users/maxvisser/Documents/ITU/master_thesis/hpc/jobs/{experiment_id}"
+    _transfer_hpc_to_local(hpc_out, out_dir)
 
 
 if __name__ == "__main__":
-    experiment_id = "context30_1211_1017"
-    job_id = 459
+    experiment_id = "context30_1211_1114"
+    job_id = 462
 
+    # Create out folder
     path = f"/Users/maxvisser/Documents/ITU/master_thesis/hpc/jobs/{experiment_id}"
     if not os.path.exists(path):
         os.makedirs(path)
