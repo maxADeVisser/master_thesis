59f852331752a2a6f0b2d8fe5df90bce7336fba9
maxADeVisser <m.madv96@gmail.com>
2024-11-19 16:15:54 +0100

    wip fiftyone setup

--- analysis/analyse_embeddings.py
+++ analysis/analyse_embeddings.py
@@ -1,56 +1,32 @@
-from typing import Literal
-
 import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
 import plotly.io as pio
 import seaborn as sns
-import umap
 from plotly.express import scatter_3d
-from sklearn.manifold import TSNE
 
-from project_config import SEED
+from project_config import env_config
 
 # SCRIPT PARAMS ---------
 # TODO remove TNSE stuff from here. this should be in the get_embeddings.py script
 fold = 3
 experiment_id = "c30_3D_1711_1513"
 hold_out = False
-reduction_algo: Literal["umap", "tsne"] = "tsne"
 # -----------------------
 
 hold_indicator = "_holdout" if hold_out else ""
-out = f"out/embeddings/{experiment_id}/fold{fold}"
-model_embeddings = np.load(f"{out}/embeddings{hold_indicator}.npy")
-labels = np.load(f"{out}/labels{hold_indicator}.npy")
-print(model_embeddings.shape, labels.shape)
-
-# Compute the 2D embeddings:
-if reduction_algo == "tsne":
-    tnse = TSNE(n_components=2, perplexity=30, random_state=SEED)
-    dim_reduced_embeddings = tnse.fit_transform(model_embeddings)
-elif reduction_algo == "umap":
-    reducer = umap.UMAP(random_state=SEED)
-    dim_reduced_embeddings = reducer.fit_transform(model_embeddings)
-
-np.save(
-    f"{out}/{reduction_algo}_embeddings{hold_indicator}.npy", dim_reduced_embeddings
-)
+out = f"{env_config.OUT_DIR}/embeddings/{experiment_id}/fold{fold}"
 
 # LOAD - these are 2D embeddings:
-# dim_reduced_embeddings = np.load(
-#     f"{out}/{reduction_algo}_embeddings{hold_indicator}.npy"
-# )
-
-tnse_df = pd.DataFrame.from_records(dim_reduced_embeddings)
-tnse_df["label"] = labels
+dim_reduced_embeddings = pd.read_csv(
+    "out/embeddings/c30_3D_1711_1513/fold3/embeddings_df.csv"
+).query("dataset == 'train'")
 
 # plot the tnse embeddings
 plt.figure(figsize=(10, 10))
 sns.scatterplot(
-    data=tnse_df,
-    x=0,
-    y=1,
+    data=dim_reduced_embeddings,
+    x="x_embed",
+    y="y_embed",
     hue="label",
     palette="tab10",
     alpha=0.8,
@@ -58,14 +34,14 @@ sns.scatterplot(
     # size="subtlety",
 )
 plt.legend(loc="upper right", title="True\nMalignancy\nScore")
-plt.title(f"{reduction_algo} embeddings of Nodule ROIs")
-plt.savefig(f"{out}/{reduction_algo}_embeddings_plot{hold_indicator}.png")
+plt.title(f"tnse embeddings of Nodule ROIs")
+plt.savefig(f"{out}/tnse_embeddings_plot{hold_indicator}.png")
 plt.axis("off")
 plt.show()
 
 # plot 3D
 fig = scatter_3d(
-    tnse_df,
+    dim_reduced_embeddings,
     x=0,
     y=1,
     z=2,
@@ -84,6 +60,6 @@ pio.write_html(
 fig.show()
 
 
-# TODO
+# TODO ???
 # evaluate clusters using silhouette score and other clustering metrics
 # potentially use the elbow method to determine the optimal number of clusters
--- analysis/fiftyone_app.py
+++ analysis/fiftyone_app.py
@@ -7,13 +7,13 @@ import pandas as pd
 
 from project_config import env_config
 
-# SCRIPT PARAMS:
+# --- SCRIPT PARAMS ---
 context_size: Literal[30, 50, 70] = 30
 experiment_id = "c30_3D_1711_1513"
 fold = 3
-# ---
+# ---------------------
 
-nodule_roi_jpg_path = (
+nodule_roi_jpg_dir = (
     f"{env_config.PROJECT_DIR}/data/middle_slice_images_c{context_size}"
 )
 
@@ -27,12 +27,7 @@ embeddings_df_path = (
 pred_nodule_df = pd.read_csv(pred_df_path).set_index("nodule_id")
 embeddings_df = pd.read_csv(embeddings_df_path).set_index("nodule_id")
 
-# load embeddings
-# TODO improve how embeddings are computed!
-# embeddings_path = f"{env_config.OUT_DIR}/embeddings/{experiment_id}/fold{fold}"
-# embeddings = np.load(f"{embeddings_path}/tnse_embeddings.npy")
-# labels = np.load(f"{embeddings_path}/labels.npy")
-
+# TODO do not delete the dataset everytime. We need to load the existing one instead.
 dataset_name = f"C{context_size}_Nodule_ROIs"
 if dataset_name in fo.list_datasets():
     fo.delete_dataset(dataset_name)
@@ -41,7 +36,7 @@ if dataset_name in fo.list_datasets():
 # --- CREATE DATASET ---
 # (Only run once - stores in a MongoDB database)
 dataset = fo.Dataset.from_images_patt(
-    images_patt=f"{nodule_roi_jpg_path}/*.jpg", name=dataset_name
+    images_patt=f"{nodule_roi_jpg_dir}/*.jpg", name=dataset_name
 )
 # dataset = fo.load_dataset(dataset_name)
 
@@ -51,7 +46,7 @@ for sample in dataset:
     row_embed = embeddings_df.loc[nodule_id]
 
     # Store classification in a field name of your choice
-    sample["nodule_id"] = pred_nodule_df.loc[nodule_id].name
+    sample["nodule_id"] = nodule_id
     sample["malignancy_consensus"] = fo.Classification(
         label=str(row["malignancy_consensus"])
     )
@@ -66,17 +61,18 @@ for sample in dataset:
     sample["ann_mean_diameter"] = row["ann_mean_diameter"]
     sample.save()
 
+# Map sample IDs to nodule ID annoying and complex mapping ...
 nodule_id_sample_id_mapping = {sample["nodule_id"]: sample.id for sample in dataset}
 nodule_id_sample_id_mapping = (
     pd.DataFrame.from_dict(nodule_id_sample_id_mapping, orient="index")
     .reset_index()
     .rename(columns={"index": "nodule_id", 0: "sample_id"})
 )
+
 fiftyone_df = pd.merge(pred_nodule_df, nodule_id_sample_id_mapping, on="nodule_id")
 fiftyone_df = pd.merge(fiftyone_df, embeddings_df, on="nodule_id")
 
-fiftyone_df[["sample_id", "x_embed", "y_embed"]]
-
+# get the format needed for the compute_visualization function:
 sample_id_embedding_mapping = dict(
     zip(fiftyone_df["sample_id"], zip(fiftyone_df["x_embed"], fiftyone_df["y_embed"]))
 )
@@ -90,5 +86,5 @@ fob.compute_visualization(
 )
 
 # --- LAUNCH APP ---
-session = fo.launch_app(dataset=dataset)
+session = fo.launch_app(dataset=dataset, port=5151)
 session.wait()
