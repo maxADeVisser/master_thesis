84535dd2c23102e3a4365e31643ecc26fbd2b496
maxADeVisser <m.madv96@gmail.com>
2024-11-22 12:29:12 +0100

    debugging precompute function on the hpc

--- data/precompute_nodule_dataset.py
+++ data/precompute_nodule_dataset.py
@@ -27,37 +27,45 @@ with open("experiment_analysis_parameters.json", "r") as f:
     config = json.load(f)
 
 # --- SCRIPT PARAMS ---
-context_window: list[int] = config["context_sizes"]
-dimensionality: list[str] = config["dimensionalities"]
+context_windows: list[int] = config["precompute_nodule_dataset"]["context_sizes"]
+dimensionalities: list[str] = config["precompute_nodule_dataset"]["dimensionalities"]
 holdout_set: bool = config["holdout_set"]
 # ---------------------
 
 dataset_version: Literal["hold_out", "train"] = "hold_out" if holdout_set else "train"
+_holdout_indicator = "hold_out" if dataset_version == "hold_out" else ""
 
 # Load the preprocessed nodule dataframe
 PROJECT_DIR = os.getenv("PROJECT_DIR")
-_holdout_indicator = "hold_out" if dataset_version == "hold_out" else ""
 if dataset_version == "train":
     nodule_df = pd.read_csv(env_config.processed_nodule_df_file)
 else:
     nodule_df = pd.read_csv(env_config.hold_out_nodule_df_file)
-for cws in context_window:
+
+# Cast the string representations of the consensus bounding boxes to tuples:
+for cws in context_windows:
     nodule_df[f"consensus_bbox_{cws}"] = nodule_df[f"consensus_bbox_{cws}"].apply(
         ast.literal_eval
     )
 
-# create the directories for the precomputed ROIs
-for cws in context_window:
-    for dim in dimensionality:
+# Create the directories for the precomputed ROIs:
+# First check if the directories already exist:
+for cws in context_windows:
+    for dim in dimensionalities:
         OUT_DIR = (
             f"{PROJECT_DIR}/data/precomputed_rois_{cws}C_{dim}{_holdout_indicator}"
         )
-        if not os.path.exists(OUT_DIR):
-            os.makedirs(OUT_DIR)
-        else:
+        if os.path.exists(OUT_DIR):
             raise FileExistsError(f"{OUT_DIR} already exists. Reset first")
+# Then create the directories:
+for cws in context_windows:
+    for dim in dimensionalities:
+        OUT_DIR = (
+            f"{PROJECT_DIR}/data/precomputed_rois_{cws}C_{dim}{_holdout_indicator}"
+        )
+        os.makedirs(OUT_DIR)
 
-# loop over nodules and precompute the ROIs at different context windows sizes:
+# loop over nodules and precompute the ROIs at the specified context windows and dimensionalities:
 for _, row in tqdm(
     nodule_df.iterrows(),
     desc="Precomputing ROIs",
@@ -68,7 +76,7 @@ for _, row in tqdm(
     malignancy_consensus = row["malignancy_consensus"]
 
     # For each context, precompute the ROI
-    for cws in context_window:
+    for cws in context_windows:
         x_bounds, y_bounds, z_bounds = row[f"consensus_bbox_{cws}"]
         nodule_roi = scan[
             x_bounds[0] : x_bounds[1],
@@ -83,13 +91,27 @@ for _, row in tqdm(
 
         nodule_roi: torch.Tensor = torch.from_numpy(nodule_roi).unsqueeze(0).float()
 
-        for dim in dimensionality:
+        for dim in dimensionalities:
             OUT_DIR = (
                 f"{PROJECT_DIR}/data/precomputed_rois_{cws}C_{dim}{_holdout_indicator}"
             )
             if dim == "2.5D":
                 nodule_roi = transform_3d_to_25d(nodule_roi)
             nodule_roi = clip_and_normalise_volume(nodule_roi)
+
+            if dim == "3D":
+                assert nodule_roi.shape == (
+                    1,
+                    cws,
+                    cws,
+                    cws,
+                ), f"Incorrect shape for 3D ROI: {nodule_roi.shape}. Should be ({(1, cws, cws, cws)})"
+            elif dim == "2.5D":
+                assert nodule_roi.shape == (
+                    3,
+                    cws,
+                    cws,
+                ), f"Incorrect shape for 2.5D ROI: {nodule_roi.shape}. Should be ({(3, cws, cws)})"
 
             torch.save(
                 (nodule_roi, malignancy_consensus), f"{OUT_DIR}/{row['nodule_id']}.pt"
