eeaa3ff4ab12a7a59e7cd0d4f1cacdd00cb187bd
maxADeVisser <m.madv96@gmail.com>
2024-11-27 12:37:15 +0100

    update again to fiftyone

--- analysis/fiftyone_app/export.py
+++ analysis/fiftyone_app/export.py
@@ -13,11 +13,11 @@ with open("experiment_analysis_parameters.json", "r") as f:
     config = ExperimentAnalysis.model_validate(json.load(f))
 
 experiments = ["c30_25D_2411_1543", "c50_25D_2411_1812", "c70_25D_2411_1705"]
-
-fold = config.analysis.fold
+fold = 0
 
 for experiment_id in tqdm(experiments, total=len(experiments), desc="Exporting tags"):
-    export_dir = f"/Users/newuser/Documents/ITU/master_thesis/out/fiftyone/{experiment_id}_fold{fold}"
+    export_dir = f"/Users/newuser/Documents/ITU/master_thesis/analysis/fiftyone_app/app_annotations"
+    file_name = f"{experiment_id}_fold{fold}.csv"
     dataset_name = f"{experiment_id}"
     # ----
 
@@ -35,7 +35,7 @@ for experiment_id in tqdm(experiments, total=len(experiments), desc="Exporting t
         for sample in dataset
     ]
 
-    with open(f"{export_dir}/tags_export.csv", "w", newline="") as f:
+    with open(f"{export_dir}/{file_name}", "w", newline="") as f:
         writer = csv.DictWriter(
             f, fieldnames=["sample_id", "filepath", "tags", "nodule_ids"]
         )
