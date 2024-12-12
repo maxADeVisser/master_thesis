d4af6524df6fadc946c2b01706c3453b0de270d6
maxADeVisser <m.madv96@gmail.com>
2024-12-12 14:53:11 +0100

    cleanup

--- utils/visualisation.py
+++ utils/visualisation.py
@@ -7,10 +7,12 @@ import numpy as np
 import pandas as pd
 import pylidc as pl
 import seaborn as sns
-from pylidc.utils import consensus, find_contours, volume_viewer
+from pylidc.utils import volume_viewer
+from tqdm import tqdm
 
-# from project_config import env_config, pipeline_config
+from project_config import env_config, pipeline_config
 from utils.data_models import load_fold_from_json
+from utils.utils import load_scan
 
 
 def plot_val_error_distribution(
@@ -233,86 +235,6 @@ def plot_fold_results(
     plt.show()
 
 
-def show_segmentation_consensus(
-    scan: pl.Scan, nodule_idx: int, clevel: float = 0.5
-) -> None:
-    """
-    Display the consensus segmentation for a nodule in a scan.
-
-    Args:
-        @scan: The pylidc.Scan object.
-        @nodule_idx: The index of the nodule in the scan.
-        @clevel: The consensus fraction. For example, if clevel=0.5, then a voxel will have value 1 (True) in the returned boolean volume when at least 50% of the annotations have a value of 1 at that voxel, and 0 (False) otherwise.
-    Source: https://pylidc.github.io/tuts/consensus.html
-    """
-    # Query for a scan, and convert it to an array volume.
-    vol = scan.to_volume(verbose=False)
-
-    # Cluster the annotations for the scan, and grab one.
-    nods = scan.cluster_annotations()
-    anns = nods[nodule_idx]
-
-    # Perform a consensus consolidation and 50% agreement level.
-    # We pad the slices to add context for viewing.
-    cmask, cbbox, masks = consensus(
-        anns, clevel=clevel, pad=[(20, 20), (20, 20), (0, 0)]
-    )
-
-    # Get the central slice of the computed bounding box.
-    k = int(0.5 * (cbbox[2].stop - cbbox[2].start))
-
-    # Set up the plot.
-    fig, ax = plt.subplots(1, 1, figsize=(5, 5))
-    ax.imshow(vol[cbbox][:, :, k], cmap=plt.cm.gray, alpha=0.5)
-
-    # Plot the annotation contours for the kth slice.
-    colors = ["r", "g", "b", "y"]
-    for j in range(len(masks)):
-        for c in find_contours(masks[j][:, :, k].astype(float), 0.5):
-            label = "Annotation %d" % (j + 1)
-            plt.plot(c[:, 1], c[:, 0], colors[j], label=label)
-
-    # Plot the 50% consensus contour for the kth slice.
-    for c in find_contours(cmask[:, :, k].astype(float), 0.5):
-        plt.plot(c[:, 1], c[:, 0], "--k", label="50% Consensus")
-
-    ax.axis("off")
-    ax.legend()
-    plt.tight_layout()
-    plt.show()
-
-
-# def plot_scan_hounsfield_histogram(
-#     pids: list[str],
-#     bounds: tuple[int, int] = (
-#         pipeline_config.dataset.clipping_bounds[0],
-#         pipeline_config.dataset.clipping_bounds[1],
-#     ),
-#     bins: int = 80,
-# ) -> None:
-#     """
-#     Plots the histogram of Hounsfield units in a CT scan
-#     @scan - a 3D numpy array of the CT scan
-#     """
-#     scan = get_scans_by_patient_id(pids[0], to_numpy=True)
-
-#     for pid in tqdm(range(1, len(pids)), desc="Loading Scans"):
-#         next_pid_scan = get_scans_by_patient_id(
-#             env_config.patient_ids[pid], to_numpy=True
-#         )
-#         scan = np.concatenate([scan, next_pid_scan], axis=2)
-
-#     print("Plotting...")
-#     plt.title(f"Hounsfield Units (HU) Distribution for {len(pids)} Scans")
-#     plt.hist(scan.flatten(), bins=bins, color="c")
-#     plt.axvline(x=bounds[0], color="r", linestyle="--", label="Lower Bound")
-#     plt.axvline(x=bounds[1], color="b", linestyle="--", label="Upper Bound")
-#     plt.legend()
-#     plt.xlabel("Hounsfield Units (HU)")
-#     plt.ylabel("Frequency")
-#     plt.show()
-
-
 def plot_slices(
     num_rows: int,
     num_cols: int,
@@ -321,13 +243,10 @@ def plot_slices(
     slices: np.ndarray,
     save_path: str | None = None,
 ) -> None:
-    """Plot a montage of @num_rows * @num_cols CT slices.
-    Source: https://keras.io/examples/vision/3D_image_classification/"""
     assert (
         end_scan_idx - start_scan_idx == num_rows * num_cols
     ), "The number of selected slices must equal the number of rows and columns multiplied"
 
-    # the number of selected slices must equal the number of rows and columns multiplied:
     slices = slices[start_scan_idx:end_scan_idx]
     _, width, height = slices.shape
 
@@ -367,17 +286,6 @@ def visualise_scan_interactively(pid: str) -> None:
 # %%
 if __name__ == "__main__":
     # TESTING
-    """Need to Load a scan from a folder and returns a 3D numpy array of all the scans stacked together
-    in the shape: (n_slices, width, height):"""
-    # slices = ...
-    # plot_slices(
-    #     num_rows=4,
-    #     num_cols=10,
-    #     start_scan_idx=30,
-    #     end_scan_idx=70,
-    #     slices=slices,
-    #     # save_path="out/test.png",
-    # )
 
     # # TESTING
     # plot_scan_hounsfield_histogram(env_config.patient_ids[:50], bins=80)
