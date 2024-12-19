94d50d8cc298cd094d4fb04a54047acdb2305383
maxADeVisser <m.madv96@gmail.com>
2024-12-19 10:34:24 +0100

    updated annotations and plots

--- analysis/annotater_agreement.py
+++ analysis/annotater_agreement.py
@@ -1,37 +1,35 @@
-
 import pandas as pd
 
-from google.colab import files
-uploaded = files.upload()
 
-# Load the file from the uploaded dictionary
-import pandas as pd
-data = pd.read_csv(list(uploaded.keys())[0])
+def compute_kendall_w(annotation_data: pd.DataFrame) -> pd.DataFrame:
+    """Computes Kendall's W assuming that there are no rank ties."""
 
-# Check the data
-print(data.head())
+    # n: number of items (nodules) and m: number of raters (annotations):
+    n, m = annotation_data.shape
 
-# We need to transpose the data to have raters as rows and items (nodules) as columns
-data_t = data.T
+    # Rank the ratings for each subject:
+    ranked_data = annotation_data.rank(axis=0, method="average")
 
-# Calculate Kendall's W - Items and raters
-n_items = data.shape[0]  # number of items (nodules)
-n_raters = data.shape[1]  # number of raters (annotations)
+    # Compute the sum of ranks for each item:
+    sum_of_total_ranks = ranked_data.sum(axis=1)  # R_i
 
-# Rank the ratings for each subject
-ranked_data = data_t.rank(axis=1)
+    # Compute the mean of the sum of total ranks:
+    mean_of_ranks = sum_of_total_ranks.mean()  # \bar{R}
 
-# Compute the sum of ranks for each item
-sum_of_ranks = ranked_data.sum(axis=0)
+    # Compute the sum of squared deviations from the mean rank sum:
+    S = ((sum_of_total_ranks - mean_of_ranks) ** 2).sum()
 
-# Compute the mean of the sum of ranks
-mean_of_ranks = sum_of_ranks.mean()
+    #  Calculate Kendall's W:
+    W = (12 * S) / (m**2 * (n**3 - n))
 
-# Compute the sum of squared deviations from the mean rank sum
-S = sum((sum_of_ranks - mean_of_ranks)**2)
+    return W
 
-#  Calculate Kendall's W
-W = (12 * S) / (n_raters**2 * (n_items**3 - n_items))
 
-# Output the result
-print(f"Kendall's W (Coefficient of Concordance): {W}")
+if __name__ == "__main__":
+    anno_2 = pd.read_csv("out/annotation_files/malignancy_2.csv")
+    anno_3 = pd.read_csv("out/annotation_files/malignancy_3.csv")
+    anno_4 = pd.read_csv("out/annotation_files/malignancy_4.csv")
+
+    compute_kendall_w(anno_2)
+    compute_kendall_w(anno_3)
+    compute_kendall_w(anno_4)
